f1ef3dd4eaede5ec327718898b88d1206297e22d
the-redback <maruf.2hin@gmail.com>
2021-02-15 06:32:00 -0600

    divided endpoints into subgroups

--- src/main/java/com/cloudhubs/istiologparser/GVGenerator.java
+++ src/main/java/com/cloudhubs/istiologparser/GVGenerator.java
@@ -23,14 +23,28 @@ public class GVGenerator {
         StringBuilder graph = new StringBuilder();
         graph.append("digraph cil_rad {").append("\n");
         graph.append("rankdir = LR;").append("\n");
-        graph.append("node [shape=box];").append("\n");
+        graph.append("node [shape=oval];").append("\n");
 
         int clusterIndex = 0;
 
-        FunctionsMap.forEach((nestedMap,value) -> {
+        FunctionsMap.forEach((nestedMap, value) -> {
             nestedMap.forEach((node1, secondNestedMap) -> {
                 secondNestedMap.forEach((node2, node3) -> {
-                    String link = String.format("%s  -> %s [ label = %s ];", addDoubleQuotations(node1), addDoubleQuotations(node2), addDoubleQuotations(String.valueOf(value) + " "+ node3));
+
+                    String subGraph = formatNodeName(node2);
+                    String endPoint = formatNodeName(node3);
+                    String subEndPoint = subGraph + "_" + endPoint;
+
+
+                    graph.append("\n");
+
+                    graph.append(String.format("subgraph cluster_%s {", subGraph)).append("\n");
+                    graph.append("label=").append(addDoubleQuotations(node2)).append(";").append("\n");
+                    graph.append(subEndPoint).append(String.format("[label=%s]",addDoubleQuotations(node3))).append(";").append("\n");
+                    graph.append("}").append("\n");
+                    graph.append("\n");
+
+                    String link = String.format("%s  -> %s [label = %s ];", addDoubleQuotations(node1), addDoubleQuotations(subEndPoint), addDoubleQuotations(String.valueOf(value)));
                     graph.append(link).append("\n");
                 });
             });
@@ -46,7 +60,7 @@ public class GVGenerator {
         }
     }
 
-//    private static Map<String, Set<String>> getClusters(RadResponseContext radResponseContext) {
+    //    private static Map<String, Set<String>> getClusters(RadResponseContext radResponseContext) {
 //        Map<String, Set<String>> clusters = new HashMap<>();
 //
 //        for (RestFlow restFlow : radResponseContext.getRestFlowContext().getRestFlows()) {
@@ -64,6 +78,10 @@ public class GVGenerator {
 //
     private static String addDoubleQuotations(String str) {
         return "\"" + str + "\"";
+    }
+
+    private static String formatNodeName(String str) {
+        return str.replaceAll("[^a-zA-Z0-9_]", "_");
     }
 
     private static void addToMap(Map<String, Set<String>> m, String key, String value) {
